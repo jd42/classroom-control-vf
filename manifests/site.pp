759a393a2a40e33773cda6ababc3775c08736ae1
jd42 <jdeluca9@gmail.com>
2016-08-24 13:32:59 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file {'/etc/motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  comment => "Today I learned something."
+  }
+  
 }
